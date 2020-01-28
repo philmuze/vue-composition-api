233495b6b24671ffe4e2f3f58ae8315dd50e15f4
Philipp Munzert <mail@phmu.de>
2020-01-28 13:15:15 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <member-list></member-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MemberList from "@/components/MemberList";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    MemberList
   }
 };
 </script>
